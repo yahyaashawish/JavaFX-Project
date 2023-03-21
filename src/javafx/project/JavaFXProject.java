770b376ac39df986e71403588a2a526c0387035b
yahya321 <yahya0592727763@gmail.com>
2023-03-21 20:52:11 +0200

    first week implementation

---
+++ src/javafx/project/JavaFXProject.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafx.project;
+
+import View.RegisterPage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Yahya
+ */
+public class JavaFXProject extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        RegisterPage reg= new RegisterPage();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
